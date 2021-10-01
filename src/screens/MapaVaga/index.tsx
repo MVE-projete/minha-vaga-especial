69a597943ad552a46138648ffb6820c4e582ac96
MVE-projete <86355749+MVE-projete@users.noreply.github.com>
2021-10-01 16:30:44 -0300

    Add files via upload

--- src/screens/MapaVaga/index.tsx
+++ src/screens/MapaVaga/index.tsx
@@ -1,37 +1,41 @@
 import MapView from 'react-native-maps';
 import React, { useState, useEffect } from 'react';
-import { View, Text,StyleSheet, Button, ScrollView, Dimensions } from 'react-native';
+import { View, Text,StyleSheet, Button, ScrollView, Dimensions, TextInput, Platform, Modal } from 'react-native';
 import firebase from '../../firebaseConnection';
 import { BotBig, BotMenor } from '../../components/registerButton';
 
 const { height, width } = Dimensions.get('window');
+let cor;
 
 export function MapaVaga({navigation}){
   state = { //colocar tres estados do pino (tres cores) E FAZER A AUTENTICÇÃO e APK
     places: [
       {
-        id: 1,
+        id: 0,
         rua: 'Adélino Carneiro Pinto',
         bairro: 'Centro',
         description: 'Estacionamento Unissul',
         latitude: -22.25473293664138, 
         longitude: -45.70466075394644,
+        codigo: 123,
       },
       {
-        id: 2,
+        id: 1,
         rua: 'Adélino Carneiro Pinto',
         bairro: 'Centro',
         description: 'Estacionamento Unissul',
         latitude: -22.254762104945637, 
         longitude: -45.7047271386185,
+        codigo: 321,
       },
       {
-        id: 3,
+        id: 2,
         rua: 'A. José Cleto Duarte',
         bairro: 'Centro',
         description: 'Estacionamento Unissul',
         latitude: -22.254351255893518,
-        longitude:  -45.7053063842081
+        longitude:  -45.7053063842081,
+        codigo: 132, 
       }
     ],
   };
@@ -41,6 +45,31 @@ export function MapaVaga({navigation}){
   
   const [aux, setAux] = useState(1);
   const [sensorv, setSensorv] = useState();
+  const [validation, setValidation] = useState();
+  const [validstate, setValidstate] = useState(0);
+  let isModalVisible = false;
+
+
+  function valid(){
+    
+    if(validation == 123)
+    {
+      firebase.database().ref('verificacao').update({
+        valor: 1,
+      })
+      alert("Validação feita com sucesso");
+      setValidstate(1);
+    }
+    else
+    {
+      alert("Erro na validação!")
+      setValidstate(0);
+    }
+    setAux(0);
+        setTimeout(function() {
+          setAux(1);
+        }, 10);
+  }
 
   useEffect(() => { //Função pra puxar o valor do sensor do firebase (armazenada na variável 'sensorv')
 
@@ -61,22 +90,27 @@ export function MapaVaga({navigation}){
     dados();
   }, []);
 
-  if (sensorv == 1)
+  if (sensorv == 1 && validstate == 1)
+  {
+    color = 'red';
+  }
+  else if (sensorv == 0 && validstate == 0)
   {
     color = 'green';
   }
-  else if (sensorv == 0)
+  else if(sensorv == 1 && validstate == 0)
   {
-    color = 'blue';
+    color = 'yellow';
+  }
+  else if(sensorv == 0 && validstate == 1)
+  {
+    color = 'orange';
   }
 
-  function mudaPin(){
-    setAux(0);
-    setTimeout(function() {
-      setAux(1);
-    }, 1);
+  
+  cor = ['red', 'red', color];
 
-  }
+  
 
   const { latitude, longitude } = this.state.places[0];
 
@@ -95,18 +129,20 @@ export function MapaVaga({navigation}){
         rotateEnabled={false}
     >
 
-    {aux == 1 ? //Em todos os dois casos ele coloca o pincolor com o valor de 'color'. E o color varia conforme varia o sensorv (linha 27)
+    {aux == 1 ? 
+      //Em todos os dois casos ele coloca o pincolor com o valor de 'color'. E o color varia conforme varia o sensorv (linha 27)
       this.state.places.map(place => (
         <MapView.Marker 
                 title={"Vaga " + place.id}
                 description={place.description}
                 ref={mark => place.mark = mark}
                 key={place.id}
-                pinColor={color}
+                pinColor={cor[place.id]}
                 coordinate={{
                   latitude: place.latitude,
                   longitude: place.longitude,
               }}
+              
         />
     ))
     :
@@ -115,8 +151,13 @@ export function MapaVaga({navigation}){
           </>
     )}
 
+   
+
+   
     
   </MapView>
+  
+
 
   <ScrollView 
     style={styles.placesContainer}
@@ -143,6 +184,7 @@ export function MapaVaga({navigation}){
 
       }, 1000);
 
+      
 
     }}> 
 
@@ -160,8 +202,16 @@ export function MapaVaga({navigation}){
         <Text style={styles.text2}>
           Bairro: {place.bairro}
         </Text> 
+        <TextInput
+        style={{backgroundColor: '#B3E0FF', borderRadius: 5, marginRight: 20, marginLeft: 20, height: 40}}
+        placeholder="Código da vaga"
+        onChangeText={texto => {
+            setValidation(texto)
+        }}/>
         <BotBig 
-          title="Validar vaga"/>
+          title="Validar vaga"
+          onPress={valid}/>
+        
 
         </View>
 
@@ -192,12 +242,12 @@ MapView:{
 
 placesContainer:{
   width: '100%',
-  maxHeight: 200
+  maxHeight: 250
 
 },
 place:{
   width: width -40,
-  maxHeight: 200,
+  maxHeight: 250,
   backgroundColor: '#FFF',
   marginHorizontal: 20,
   borderRadius: 12
